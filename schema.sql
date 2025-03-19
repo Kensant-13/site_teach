d89cbd67abac53af4857b4db640ec62d7e674fda
Kensant-13 <kensant.live@gmail.com>
2025-03-19 21:31:22 +0700

    Обновление БД | schema.sql

--- schema.sql
+++ schema.sql
@@ -8,9 +8,6 @@ CREATE TABLE Проекты (
                          id INT PRIMARY KEY AUTO_INCREMENT,
                          название VARCHAR(255) NOT NULL,
                          описание TEXT,
-                         дата_начала DATE,
-                         дата_окончания DATE,
-                         статус ENUM('планируется', 'в процессе', 'завершен'),
                          пользователь_id INT,
                          FOREIGN KEY (пользователь_id) REFERENCES Пользователи(id)
 );
@@ -18,9 +15,9 @@ CREATE TABLE Проекты (
 CREATE TABLE Задачи (
                         id INT PRIMARY KEY AUTO_INCREMENT,
                         тема VARCHAR(255) NOT NULL,
-                        статус ENUM('новая', 'в работе', 'завершена'),
-                        приоритет ENUM('низкий', 'средний', 'высокий'),
                         дата_выполнения DATE,
+                        пользователь_id INT,
+                        FOREIGN KEY (пользователь_id) REFERENCES Пользователи(id),
                         проект_id INT,
                         FOREIGN KEY (проект_id) REFERENCES Проекты(id)
 );
