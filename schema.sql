20eff89f1ffa854ba0490d580ac2a62f7b883bbf
Kensant-13 <kensant.live@gmail.com>
2025-03-28 16:40:02 +0700

    Обновление запроса на создание бд + запрос на заполнение бд | schema.sql & queries.sql

--- schema.sql
+++ schema.sql
@@ -1,23 +1,29 @@
+CREATE DATABASE site_teach;
+USE site_teach;
+
 CREATE TABLE Пользователи (
                               id INT PRIMARY KEY AUTO_INCREMENT,
-                              имя VARCHAR(100) NOT NULL,
+                              имя VARCHAR(100) NOT NULL COLLATE 'utf8_general_ci',
                               email VARCHAR(100) UNIQUE NOT NULL,
+                              пароль VARCHAR(60) NOT NULL,
                               дата_регистрации DATETIME DEFAULT CURRENT_TIMESTAMP
-);
+) COLLATE='utf8_general_ci';
 CREATE TABLE Проекты (
                          id INT PRIMARY KEY AUTO_INCREMENT,
-                         название VARCHAR(255) NOT NULL,
-                         описание TEXT,
+                         название VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+                         site_title VARCHAR(255) NOT NULL,
+                         описание TEXT COLLATE 'utf8_general_ci',
                          пользователь_id INT,
                          FOREIGN KEY (пользователь_id) REFERENCES Пользователи(id)
-);
+) COLLATE='utf8_general_ci';
 
 CREATE TABLE Задачи (
                         id INT PRIMARY KEY AUTO_INCREMENT,
-                        тема VARCHAR(255) NOT NULL,
-                        дата_выполнения DATE,
+                        тема VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+                        дата_выполнения DATE DEFAULT NULL,
+                        статус ENUM('выполнена', 'не выполнена') DEFAULT 'не выполнена' COLLATE 'utf8_general_ci',
                         пользователь_id INT,
                         FOREIGN KEY (пользователь_id) REFERENCES Пользователи(id),
                         проект_id INT,
                         FOREIGN KEY (проект_id) REFERENCES Проекты(id)
-);
+) COLLATE='utf8_general_ci';
